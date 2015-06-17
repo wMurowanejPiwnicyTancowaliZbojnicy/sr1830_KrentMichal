c5fb47f6abada5ab8452fc1804bcf056a4d01071
wMurowanejPiwnicyTancowaliZbojnicy <krent.michal@gmail.com>
2015-06-17 20:03:20 +0200

    Zorro tu był

---
+++ zorro.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author student205
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        System.out.println("***********");
+        System.out.println("         * ");
+        System.out.println("       *   ");
+        System.out.println("     *     ");
+        System.out.println("    *      ");
+        System.out.println("  *        ");
+        System.out.println(" *         ");
+        System.out.println("***********");    
+     }
+}
